5143222bb46ba22949fccf8e0c4e735a3fd4d0a8
Marilisa Saldanha <saldanhamarilisa@gmail.com>
2023-05-29 18:33:01 -0300

    update index.html src/script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const submit = document.querySelector('.submit')
+const submit = document.querySelector('.submitButton')
 const error = document.querySelector('.error')
 const form = document.querySelector('form')
 const input = document.querySelector('input')
