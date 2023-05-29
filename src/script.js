5b552d483c57549f8fc3e728ba38b7e10f841124
Marilisa Saldanha <saldanhamarilisa@gmail.com>
2023-05-29 18:20:56 -0300

    upload files

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const submit = document.querySelector('.submit')
+const error = document.querySelector('.error')
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const label = document.querySelector('label')
+
+form.addEventListener('submit', validate)
+submit.addEventListener('click', validate)
+
+function validate(ev) {
+  ev.preventDefault()
+
+  const inputValue = input.value.trim()
+
+  if (!isEmail(inputValue)) {
+    error.style.display = "block"
+    label.style.display = "block"
+    input.classList.add('errorInput')
+  } else {
+    error.style.display = "none"
+    label.style.display = "none"
+    input.classList.remove('errorInput')
+  }
+}
+
+function isEmail(input) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)
+}
